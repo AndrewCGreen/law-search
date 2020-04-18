6f04dd4e6def355c1a9e5618003b22e43f7e3a6e
AndrewCGreen <andrewc.green@mac.com>
2020-04-18 18:06:50 -0400

    fixing unnecesary sort sh

--- data/convert_sort.sh
+++ data/convert_sort.sh
@@ -1,10 +1,3 @@
 mkdir ./csv_format
-cp ./transcripts.txt ./csv_format/transcripts.csv
-cp ./case_information.txt ./csv_format/case_information.csv
-
-mkdir ./txt_format
-mv ./transcripts.txt ./txt_format/transcripts.txt
-mv ./case_information.txt ./txt_format/case_information.txt
-
-mkdir ./scrapers
-mv ./Scraper.ipynb ./scrapers
+cp ./txt_format/transcripts.txt ./csv_format/transcripts.csv
+cp ./txt_format/case_information.txt ./csv_format/case_information.csv
